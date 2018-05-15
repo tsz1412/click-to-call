72653766848704fef81c62a4ee439474370cb944
Tsviel Zaikman <tsz1412@gmail.com>
2018-05-15 22:01:07 +0300

    ds

--- click-to-call.php
+++ click-to-call.php
@@ -47,7 +47,7 @@ if (is_admin()) { // note the use of is_admin() to double check that this is hap
 			'requires' => '3.0', // which version of WordPress does your plugin require?
 			'tested' => '3.3', // which version of WordPress is your plugin tested up to?
 			'readme' => 'README.md', // which file to use as the readme for the version number
-			'access_token' => '58e743f3e3ea18479a3a039a6b5c9a1e09bf91d7', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
+			'access_token' => '23a4acb9ac66a4f7190668e8db26999b19488f25', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
 		);
 		new WP_GitHub_Updater($ClickToCallConfig);
 	}
